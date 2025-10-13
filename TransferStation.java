0c6d0512f09b31f244439ad1e7626076268254a8
yusrafaheem <yusra.faheem@gwmail.gwu.edu>
2025-10-13 09:15:29 -0400

    Update TransferStation.java

--- TransferStation.java
+++ TransferStation.java
@@ -1,6 +1,10 @@
+// TransferStation.java
+
 import java.util.ArrayList;
 
 public class TransferStation extends Station {
+
+    // List to hold transfer stations connected to this transfer station
     ArrayList<Station> otherStations;
 
     public TransferStation(String line, String name) {
@@ -8,68 +12,29 @@ public class TransferStation extends Station {
         otherStations = new ArrayList<>();
     }
 
-    public void addTransferStationPrev(Station s) {
-        if (!otherStations.contains(s)) {
-            otherStations.add(s);
-            s.next = this;
-        }
+    // Add a transfer station considered "previous" (but not affecting prev/next pointers)
+    public void addTransferStationPrev(Station station) {
+        otherStations.add(station);
     }
 
-    public void addTransferStationNext(Station s) {
-        if (!otherStations.contains(s)) {
-            otherStations.add(s);
-            s.prev = this;
-        }
+    // Add a transfer station considered "next"
+    public void addTransferStationNext(Station station) {
+        otherStations.add(station);
     }
 
-    protected int tripLengthHelper(Station destination, ArrayList<Station> visited) {
-        if (this.equals(destination)) return 0;
-        if (visited.contains(this)) return -1;
-
-        visited.add(this);
-
-        if (next != null) {
-            int result = next.tripLengthHelper(destination, new ArrayList<>(visited));
-            if (result != -1) return 1 + result;
-        }
-
-        if (prev != null) {
-            int result = prev.tripLengthHelper(destination, new ArrayList<>(visited));
-            if (result != -1) return 1 + result;
-        }
-
-        for (Station s : otherStations) {
-            if (!visited.contains(s)) {
-                int result = s.tripLengthHelper(destination, new ArrayList<>(visited));
-                if (result != -1) return 1 + result;
-            }
-        }
-
-        return -1;
-    }
-
+    @Override
     public String toString() {
         String prevName = (prev == null) ? "none" : prev.name;
         String nextName = (next == null) ? "none" : next.name;
-        String status = inService ? "true" : "false";
 
-        String s = "TRANSFERSTATION " + name + ": " + line + " line, in service: " + status + ", previous station: " + prevName + ", next station: " + nextName + "\n\tTransfers: \n";
-
-        for (Station t : otherStations) {
-            String tPrev = (t.prev == null) ? "none" : t.prev.name;
-            String tNext = (t.next == null) ? "none" : t.next.name;
-            String tStatus = t.inService ? "true" : "false";
-
-            String type = "STATION";
-            if (t instanceof EndStation) type = "ENDSTATION";
-            else if (t instanceof TransferStation) type = "TRANSFERSTATION";
-
-            s += "\t" + type + " " + t.name + ": " + t.line + " line, in service: " + tStatus + ", previous station: " + tPrev + ", next station: " + tNext;
-            if (type.equals("TRANSFERSTATION")) s += "\n\tTransfers: \n\n";
-            else s += "\n";
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("TRANSFERSTATION %s: %s line, in service: %s, previous station: %s, next station: %s\n",
+                name, line, inService, prevName, nextName));
+        sb.append("\tTransfers: \n");
+        for (Station s : otherStations) {
+            sb.append("\t").append(s.toString()).append("\n");
         }
-
-        return s;
+        return sb.toString();
     }
 }
 
