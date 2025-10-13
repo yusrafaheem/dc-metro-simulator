24167273b27ef09c0636d54c4e316de81b609956
yusrafaheem <yusra.faheem@gwmail.gwu.edu>
2025-10-13 08:38:16 -0400

    Update EndStation.java

--- EndStation.java
+++ EndStation.java
@@ -1,1 +1,26 @@
+// End stations are special stations that are at the ends of a line
+public class EndStation extends Station {
+
+    public EndStation(String line, String name) {
+        super(line, name);
+    }
+
+    // This method makes the end station "end" properly by connecting
+    // previous and next to each other so the line loops around the ends
+    public void makeEnd() {
+        if (prev != null) {
+            next = prev;
+        } else if (next != null) {
+            prev = next;
+        }
+    }
+
+    // Override toString to show ENDSTATION instead of just STATION
+    public String toString() {
+        String prevName = (prev == null) ? "none" : prev.name;
+        String nextName = (next == null) ? "none" : next.name;
+        String status = inService ? "true" : "false";
+        return "ENDSTATION " + name + ": " + line + " line, in service: " + status + ", previous station: " + prevName + ", next station: " + nextName;
+    }
+}
 
