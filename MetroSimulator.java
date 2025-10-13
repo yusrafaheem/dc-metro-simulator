a571a3a4f242d2e00f563b9ecdb47bcac5fa21a6
yusrafaheem <yusra.faheem@gwmail.gwu.edu>
2025-10-13 08:43:47 -0400

    Update MetroSimulator.java

--- MetroSimulator.java
+++ MetroSimulator.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
+
 public class MetroSimulator {
-    
-    // Make all stations here as static so we can access them anywhere
+
+    // all the stations are made here
     public static EndStation va_square;
     public static Station clarendon;
     public static Station court_house;
@@ -15,31 +17,29 @@ public class MetroSimulator {
     public static EndStation woodley_park;
     public static Station dupont_circle;
     public static Station farragut_north;
-    // Metro Center is shared and already defined above
+    // metro center already here
     public static Station gallery_place;
     public static EndStation judiciary_square;
 
     public static EndStation s1;
     public static Station s2;
     public static Station s3;
-    // Metro Center again, shared
+    // metro center again
     public static Station s4;
     public static EndStation s5;
 
-    // Main method to start program and test trip lengths
-    public static void main(String[] args) {
-        initialize();      // Make stations
-        makeOrangeLine();  // Connect orange line stations
-        makeRedLine();     // Connect red line stations
-        makePurpleLine();  // Connect purple line stations
+    public static void main(String[] args){
+        initialize();  // make all stations
+        makeOrangeLine();  // connect orange line
+        makeRedLine();     // connect red line
+        makePurpleLine();  // connect purple line
 
-        // Example test: How many stops from Virginia Square to Metro Center
-        int stops = va_square.tripLength(metro_center);
+        int stops = va_square.tripLength(metro_center);  // find stops between these two
         System.out.println("There are " + stops + " stops between Virginia Square and Metro Center");
     }
 
-    // Make the stations but don't connect them yet
-    public static void initialize() {
+    // create all stations with names and colors
+    public static void initialize(){
         va_square = new EndStation("orange", "Virginia Square");
         clarendon = new Station("orange", "Clarendon");
         court_house = new Station("orange", "Court House");
@@ -64,8 +64,8 @@ public class MetroSimulator {
         s5 = new EndStation("purple", "S5");
     }
 
-    // Connect stations on the Orange Line in order
-    public static EndStation makeOrangeLine() {
+    // connect stations for orange line
+    public static EndStation makeOrangeLine(){
         va_square.connect(clarendon);
         clarendon.connect(court_house);
         court_house.connect(rosslyn);
@@ -75,42 +75,28 @@ public class MetroSimulator {
         mcpherson_square.connect(metro_center);
         metro_center.connect(federal_triangle);
         federal_triangle.connect(smithsonian);
-
-        // Make the ends of orange line loop back properly
-        va_square.makeEnd();
-        smithsonian.makeEnd();
-
         return va_square;
     }
 
-    // Connect stations on the Red Line in order
-    public static EndStation makeRedLine() {
+    // connect stations for red line
+    public static EndStation makeRedLine(){
         woodley_park.connect(dupont_circle);
         dupont_circle.connect(farragut_north);
         farragut_north.connect(metro_center);
         metro_center.connect(gallery_place);
         gallery_place.connect(judiciary_square);
-
-        // Make the ends of red line loop back properly
-        woodley_park.makeEnd();
-        judiciary_square.makeEnd();
-
         return woodley_park;
     }
 
-    // Connect stations on the Purple Line in order
-    public static EndStation makePurpleLine() {
+    // connect stations for purple line
+    public static EndStation makePurpleLine(){
         s1.connect(s2);
         s2.connect(s3);
         s3.connect(metro_center);
         metro_center.connect(s4);
         s4.connect(s5);
-
-        // Make the ends of purple line loop back properly
-        s1.makeEnd();
-        s5.makeEnd();
-
         return s1;
     }
 }
+
 
